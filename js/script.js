4e4075d383a29c853075b6d01269b504c0c1eef4
Cyrill <bazilyanixxx@gmail.com>
2023-01-28 15:07:17 +0300

    первый вариант слайдера изичный

--- js/script.js
+++ js/script.js
@@ -127,6 +127,79 @@ window.addEventListener('DOMContentLoaded', () => {
             CloseModal();
         }
     });
+
+    // СЛАЙДЕР 
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
+    let slideIndex = 1;
+
+
+    // Возвращает слайдер в начальное положение, если слайды закончились)
+    
+    showSlides(slideIndex);
+// Добавляет нули и кол-во слайдера на данный момент из всех
+ if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }   
+    
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+//      счётчик слайдера
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        } 
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Вызов модального окна после определенного времени
     // const modalTimerId = setTimeout(openModal, 1000);
 
